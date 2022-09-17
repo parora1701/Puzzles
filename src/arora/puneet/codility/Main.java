43beae41f3a4cc287780c075ce643a3930ef0362
Puneet Arora <arora.puneet777@gmail.com>
2022-09-17 21:58:58 +0200

    Added Codility Lesson 2, Cyclic Rotation Puzzle

--- src/arora/puneet/codility/Main.java
+++ src/arora/puneet/codility/Main.java
@@ -3,6 +3,7 @@ package arora.puneet.codility;
 import arora.puneet.codility.challenges.alpha2010.PrefixSet;
 import arora.puneet.codility.challenges.gamma2011.CountPalindromicSlices;
 import arora.puneet.codility.lessons.l1_iterations.BinaryGap;
+import arora.puneet.codility.lessons.l2_arrays.CyclicRotation;
 import arora.puneet.codility.lessons.l4_countingElements.FrogRiverOne;
 import arora.puneet.codility.lessons.l4_countingElements.MaxCounters;
 import arora.puneet.codility.lessons.l4_countingElements.MissingInteger;
@@ -10,10 +11,15 @@ import arora.puneet.codility.lessons.l4_countingElements.MissingInteger;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 
+
+		//------------Test Lesson 2 Cyclic Rotation Puzzle-------------
+		CyclicRotation.test_solution();
+		
 		//CountPalindromicSlices.test_solution();
 		
-		BinaryGap.test_solution();
+		//------------Test Lesson 1 Binary Gap Puzzle-------------
+		//BinaryGap.test_solution();
 	}
 
 }
