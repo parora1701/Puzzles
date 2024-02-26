9d5ba313d8533b1ba0ca8b976473cbca0eeb2ad5
Puneet Arora <arora.puneet777@gmail.com>
2024-02-26 15:47:21 +0530

    updated method to test puzzles

--- src/arora/puneet/codility/Main.java
+++ src/arora/puneet/codility/Main.java
@@ -1,6 +1,9 @@
 package arora.puneet.codility;
 
 import arora.puneet.codility.lessons.l1_iterations.BinaryGap;
+import arora.puneet.codility.lessons.l2_arrays.CyclicRotation;
+import arora.puneet.codility.lessons.l7_stacksAndQueues.Brackets;
+import arora.puneet.codility.lessons.l7_stacksAndQueues.Fish;
 
 public class Main {
 
@@ -19,7 +22,37 @@ public class Main {
 		//CyclicRotation.test_solution();
 		
 		//------------Test Lesson 1 Binary Gap Puzzle-------------
-		BinaryGap.test_solution();
+		//BinaryGap.test_solution();
+		test_solution("binarygap");
+	}
+	
+	private static void test_solution(String input) {
+		
+		String key = input.toLowerCase();
+		
+		switch (key) {
+		case "binarygap": {
+			BinaryGap.test_solution();
+			break;
+		}
+		case "cyclicrotation": {
+			CyclicRotation.test_solution();
+			break;
+		}
+		case "brackets": {
+			Brackets.test_solution();
+			break;
+			//yield type;
+		}
+		case "fish": {
+			Fish.test_solution();
+			break;
+			//yield type;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + input);
+			
+		}
 	}
 
 }
